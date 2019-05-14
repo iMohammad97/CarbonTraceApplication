4ac10eeaf0591a33b94ae762ba476e3100d15e3e
mohammad <mohammadm97i@gmail.com>
2019-05-14 19:17:51 +0430

    Initial Commit

---
+++ App.js
@@ -0,0 +1,148 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    AsyncStorage,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    StatusBar,
+    TouchableOpacity,
+    Image,
+    ActivityIndicator,
+    SafeAreaView
+} from 'react-native';
+// import { Ionicons } from '@expo/vector-icons';
+import {
+    createStackNavigator,
+    createSwitchNavigator,
+    createAppContainer,
+    createDrawerNavigator,
+    DrawerItems,
+    createBottomTabNavigator
+} from 'react-navigation';
+
+const color1 = '#45BB49';
+const color2 = '#424242';
+const color3 = '#00adf5';
+const color4 = '#ffffff';
+const color5 = '#a0a0a0';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+                <Button
+                    title="Go to Settings"
+                    onPress={() => this.props.navigation.navigate('Settings')}
+                />
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
+            </View>
+        );
+    }
+}
+
+class SettingsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
+            </View>
+        );
+    }
+}
+
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Details!</Text>
+            </View>
+        );
+    }
+}
+
+const HomeStack = createStackNavigator({
+    Home: { screen: HomeScreen },
+    Details: { screen: DetailsScreen },
+});
+
+const SettingsStack = createStackNavigator({
+    Settings: { screen: SettingsScreen },
+    Details: { screen: DetailsScreen },
+});
+
+export default createAppContainer(createBottomTabNavigator(
+    {
+        Home: { screen: HomeStack },
+        Settings: { screen: SettingsStack },
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Settings') {
+                    iconName = `ios-options${focused ? '' : '-outline'}`;
+                }
+
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                // return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    }
+));
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
