2a0bd605b83e1caaf482a3cb6e70e837a4c68aae
mohammad <mohammadm97i@gmail.com>
2019-05-30 03:30:07 +0430

    Adds routing link of the route for google maps and waze

--- src/Components/RoutingDetails.js
+++ src/Components/RoutingDetails.js
@@ -9,9 +9,10 @@ import {
     StatusBar,
     TouchableOpacity,
     Image,
+    Linking,
     ActivityIndicator,
     SafeAreaView,
-    ScrollView
+    ScrollView, Dimensions
 } from 'react-native';
 // import { Ionicons } from '@expo/vector-icons';
 import {
@@ -43,7 +44,7 @@ class Header extends React.Component {
 
 const corp_API_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjI2M2U4M2RjMzJhMWM1ZjhhZTgxNTA4YWFhNDFkM2ZiNmJmY2NlNTZmYWVjOTllMjdjMmM5ZmI4NDc2OWFiM2ViM2Q2Y2NjODgyYTQ3NDgzIn0.eyJhdWQiOiJteWF3ZXNvbWVhcHAiLCJqdGkiOiIyNjNlODNkYzMyYTFjNWY4YWU4MTUwOGFhYTQxZDNmYjZiZmNjZTU2ZmFlYzk5ZTI3YzJjOWZiODQ3NjlhYjNlYjNkNmNjYzg4MmE0NzQ4MyIsImlhdCI6MTU1ODU1MzEzNywibmJmIjoxNTU4NTUzMTM3LCJleHAiOjE1NTg1NTY3MzcsInN1YiI6IiIsInNjb3BlcyI6WyJiYXNpYyIsImVtYWlsIl19.SmwlAhP_Lwit5fDAvaJTq5w3CwGJEqB65EBOLwmndcwtnLhNZYWyw1GFj3aSpNY7tZ8GJdOhDVyGHZy99et409ytgPkGw1yuiN2X0A5xh1pOkXktIblB20fX8Kp4PXSBgCChhknnOrr_4dixq231a5G_m6hSY6AvAwe8U5s_j8zkDAyCDWYRYEhYADpfsygORJTYBSUeP_lmCdSZjutqA0dravM3yoVN-rSElMiyfOwAU3j4QQ2dbxYBRbmbgWCI4OVWqtUiYyG_rRwh6G3u-FLdLqn5-GdYiobc2-7NYJ5unRbI6f3Uev1un9iqwffESckriviquz6ot6W2kpPRJg";
 
-
+const {width, height} = Dimensions.get('window');
 export default class RoutingDetailsScreen extends React.Component {
 
     static navigationOptions = {
@@ -73,8 +74,13 @@ export default class RoutingDetailsScreen extends React.Component {
             markers: [],
             loading1: true,
             loading2: true,
+            loading3: true,
             dataSource: '',
             dataDestination: '',
+            dataRouteDistance: 0.0,
+            dataRouteDuration: 0.0,
+            dataRouteOuterLinkGoogle: '',
+            dataRouteOuterLinkWaze: '',
         };
     }
 
@@ -112,6 +118,28 @@ export default class RoutingDetailsScreen extends React.Component {
                 this.setState({
                     loading2: false,
                     dataDestination: responseJson.address_compact
+                })
+            })
+            .catch(error => console.log(error));//to catch the errors if any
+
+    };
+    loadRouteDistanceDuration = async (lat1, long1, lat2, long2) => {
+        fetch("https://map.ir/distancematrix?origins=b%2C" + String(lat1) + "%2C" + String(long1) + "&destinations=c%2C" + String(lat2) + "%2C" + String(long2) + "&sorted=true", {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'x-api-key': corp_API_key
+            },
+        })
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                    loading3: false,
+                    dataRouteDistance: (responseJson.distance[0].distance) / 1000,
+                    dataRouteDuration: (responseJson.duration[0].duration) / 60,
+                    dataRouteOuterLinkGoogle: "https://www.google.com/maps/dir/?api=1&origin="+String(lat1)+","+String(long1)+"&destination="+String(lat2)+","+String(long2),
+                    dataRouteOuterLinkWaze: "https://waze.com/ul?ll="+String(lat2)+","+String(long2)+"&z=10",
                 })
             })
             .catch(error => console.log(error));//to catch the errors if any
@@ -174,6 +202,7 @@ export default class RoutingDetailsScreen extends React.Component {
         }
         this.loadAddressesSource(markers_1_coordinate_latitude, markers_1_coordinate_longitude);
         this.loadAddressesDestination(markers_2_coordinate_latitude, markers_2_coordinate_longitude);
+        this.loadRouteDistanceDuration(markers_1_coordinate_latitude, markers_1_coordinate_longitude, markers_2_coordinate_latitude, markers_2_coordinate_longitude);
         console.log('on read (detailsScreen):', markers);
     };
 
@@ -201,12 +230,26 @@ export default class RoutingDetailsScreen extends React.Component {
                                 <Text style={styles.routesScreenRouteBoxRow1SmallText}>
                                     {this.state.dataDestination}
                                 </Text>
-                                <Text style={styles.routesScreenRouteBoxRow1BoldText}>
-                                    مسافت :
-                                </Text>
-                                <Text style={styles.routesScreenRouteBoxRow1SmallTextDistance}>
-                                    ۲۲ کیلومتر
-                                </Text>
+                                <View style={styles.rowInfo}>
+                                    <View style={styles.rowInfoContainer}>
+                                        <View style={styles.rowInfoContainerInner}>
+                                            <Text style={styles.routesScreenRouteBoxRow1SmallTextDistance}>
+                                                {this.state.dataRouteDuration.toFixed(1)} دقیقه
+                                            </Text>
+                                            <Text style={styles.routesScreenRouteBoxRow1BoldText}>
+                                                زمان :
+                                            </Text>
+                                        </View>
+                                        <View  style={styles.rowInfoContainerInner}>
+                                            <Text style={styles.routesScreenRouteBoxRow1SmallTextDistance}>
+                                                {this.state.dataRouteDistance.toFixed(2)} کیلومتر
+                                            </Text>
+                                            <Text style={styles.routesScreenRouteBoxRow1BoldText}>
+                                                مسافت :
+                                            </Text>
+                                        </View>
+                                    </View>
+                                </View>
 
                             </View>
                         </View>
@@ -214,23 +257,35 @@ export default class RoutingDetailsScreen extends React.Component {
                         <View style={styles.routesScreenRouteBoxRow2}>
                             <View style={styles.routesScreenRouteBoxRow2Container}>
 
-                                <View style={styles.routesScreenRouteBoxRow2Vehicle}>
+                                <TouchableOpacity
+                                    onPress={() => Linking.openURL(this.state.dataRouteOuterLinkGoogle).catch(err => console.error('An error occurred', err))}
+                                    style={styles.routesScreenRouteBoxRow2GoogleMapsButton}>
                                     <View style={styles.routesScreenRouteBoxRow2VehicleContainer}>
 
                                         <Image
-                                            style={{height: 15, width: 15, marginLeft: 5}}
-                                            source={require('../Assets/Icons/icPerson.png')}
+                                            style={{width: width*0.38, height: (33*width*0.38)/180}}
+                                            source={require('../Assets/Icons/icGoogleMaps.png')}
                                         />
-                                        <Text style={styles.routesScreenRouteBoxBarText}>
-                                            ۲۱ کیلومتر
-                                        </Text>
 
                                     </View>
-                                </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    onPress={() => Linking.openURL(this.state.dataRouteOuterLinkWaze).catch(err => console.error('An error occurred', err))}
+                                    style={styles.routesScreenRouteBoxRow2WazeButton}>
+                                    <View style={styles.routesScreenRouteBoxRow2WazeButtonContainer}>
 
-                                <Text style={styles.routesScreenRouteBoxRow1SmallText}>
-                                    ۵ روز پیش
-                                </Text>
+                                        <Image
+                                            style={{width: 30, height: 30, marginRight: 5}}
+                                            source={require('../Assets/Icons/icWaze.png')}
+                                        />
+                                        <Image
+                                            style={{width: width*0.2, height: (666*width*0.2)/2717}}
+                                            source={require('../Assets/Icons/icWazeName.png')}
+                                        />
+
+                                    </View>
+                                </TouchableOpacity>
+
 
                             </View>
                         </View>
@@ -245,6 +300,22 @@ export default class RoutingDetailsScreen extends React.Component {
 
 
 const styles = StyleSheet.create({
+    rowInfoContainerInner: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    rowInfoContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    rowInfo: {
+        height: 40,
+        width: '100%',
+    },
     routesScreenRouteBoxBarText: {
         fontFamily: Platform.OS === 'ios' ? "Calibri" : "CALIBRII",
         fontSize: 10,
@@ -262,17 +333,29 @@ const styles = StyleSheet.create({
         color: color4,
         // backgroundColor: color4,
     },
+    routesScreenRouteBoxRow2WazeButtonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     routesScreenRouteBoxRow2VehicleContainer: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: 'center',
     },
-    routesScreenRouteBoxRow2Vehicle: {
-        height: 20,
-        width: '30%',
-        borderRadius: 10,
-        backgroundColor: color2
+    routesScreenRouteBoxRow2GoogleMapsButton: {
+        height: 40,
+        width: '46%',
+        borderRadius: 5,
+        backgroundColor: color4
+    },
+    routesScreenRouteBoxRow2WazeButton: {
+        height: 40,
+        width: '46%',
+        borderRadius: 5,
+        backgroundColor: color4
     },
     routesScreenRouteBoxRow2Container: {
         flex: 1,
@@ -319,7 +402,7 @@ const styles = StyleSheet.create({
         fontStyle: "normal",
         marginLeft: 8,
         // width: 100,
-        // height: 40,
+        height: 30,
         // maxHeight: '100%',
         // lineHeight: 40,
         letterSpacing: 1,
@@ -336,7 +419,7 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap'
     },
     routesScreenRouteBoxRow1: {
-        height: 300,
+        height: 200,
         width: '100%',
     },
     routesScreenRouteBoxRow2: {
