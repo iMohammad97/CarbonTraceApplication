3850894aab7214433e1fb1a22724bfd51155106e
mohammad <mohammadm97i@gmail.com>
2019-06-04 18:54:32 +0430

    Imports app guide to app.js

--- App.js
+++ App.js
@@ -34,6 +34,7 @@ import {
 import ProfileScreen from "./src/Components/Profile.js";
 import AboutUsScreen from "./src/Components/AboutUs.js";
 import ContactUsScreen from "./src/Components/ContactUs.js";
+import AppGuideScreen from "./src/Components/AppGuide.js";
 import RoutingDetailsScreen from "./src/Components/RoutingDetails.js";
 import RoutingCheckInScreen from "./src/Components/RoutingCheckIn.js";
 import PointsScreen from "./src/Components/Points.js";
